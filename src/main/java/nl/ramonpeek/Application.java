89ce9f00bf1fe212b27b74223e7768342b531082
Peek,Ramon R.W.H.R <r.peek@student.fontys.nl>
2019-11-02 17:59:15 +0100

    Returned to hello world

--- src/main/java/nl/ramonpeek/Application.java
+++ src/main/java/nl/ramonpeek/Application.java
@@ -4,9 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Test pipeline!");
-        System.out.println("Test pipeline!");
-        System.out.println("Test pipeline!");
     }
 
 }
