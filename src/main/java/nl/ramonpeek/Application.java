f1ef33ddc351f8bbf9d2f9ed31a6698d84edd35f
Peek,Ramon R.W.H.R <r.peek@student.fontys.nl>
2019-11-02 18:21:59 +0100

    Updated version to 1.7

--- src/main/java/nl/ramonpeek/Application.java
+++ src/main/java/nl/ramonpeek/Application.java
@@ -3,7 +3,7 @@ package nl.ramonpeek;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!!");
     }
 
 }
